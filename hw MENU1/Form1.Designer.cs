b960793ac5e46c06a0062c7a96322a3808bbb304
Vardan <Vardan@COMPUTER>
2022-10-23 23:36:39 +0300

    Добавьте файлы проекта.

---
+++ hw MENU1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace hw_MENU1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.topLevelMenu = new System.Windows.Forms.TextBox();
+            this.subItem = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить пункт в меню";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Добавить подпункт в меню";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(389, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // topLevelMenu
+            // 
+            this.topLevelMenu.Location = new System.Drawing.Point(215, 81);
+            this.topLevelMenu.Multiline = true;
+            this.topLevelMenu.Name = "topLevelMenu";
+            this.topLevelMenu.Size = new System.Drawing.Size(162, 29);
+            this.topLevelMenu.TabIndex = 4;
+            // 
+            // subItem
+            // 
+            this.subItem.Location = new System.Drawing.Point(215, 124);
+            this.subItem.Multiline = true;
+            this.subItem.Name = "subItem";
+            this.subItem.Size = new System.Drawing.Size(162, 29);
+            this.subItem.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 317);
+            this.Controls.Add(this.subItem);
+            this.Controls.Add(this.topLevelMenu);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox topLevelMenu;
+        private System.Windows.Forms.TextBox subItem;
+    }
+}
+
---
+++ hw MENU1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hw_MENU1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(topLevelMenu.Text))
+            {
+                ToolStripItem mainMenuItem = new ToolStripDropDownButton(topLevelMenu.Text);
+                menuStrip1.Items.Add(mainMenuItem);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(subItem.Text))
+            {
+                foreach(ToolStripDropDownButton item in menuStrip1.Items)
+                {
+                    if (item.Text == topLevelMenu.Text)
+                        item.DropDownItems.Add(subItem.Text);
+                }
+            }
+        }
+    }
+}
